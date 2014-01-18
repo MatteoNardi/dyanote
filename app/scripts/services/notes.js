7599a460b41a94add445a29adc4a7f7071f0aa3e
Matteo Nardi <commits@matteonardi.org>
2014-01-18 14:50:32 +0100

    Implement notes service

--- app/scripts/services/notes.js
+++ app/scripts/services/notes.js
@@ -2,6 +2,49 @@
 
 angular.module('dyanote')
 
-.service('notes', function () {
-  // AngularJS will instantiate a singleton by calling "new" on this function
+// notes handles comunication with the REST service and notes handling.
+.service('notes', function ($resource, $q, auth, SERVER_CONFIG) {
+  
+  // All our notes
+  var notes = {};
+  var rootNoteId;
+
+  // $resource wrapper around our REST APIs.
+  var NoteResource;
+
+  // Load all notes from server with the currently logged in user.
+  this.loadAll = function () {
+    NoteResource = $resource(SERVER_CONFIG.apiUrl + 'users/:user/pages/:id',
+                            { user: auth.getEmail() });
+    var deferred = $q.defer();
+    NoteResource.query(function (result) {
+      for (var i = 0; i < result.length; i++) {
+        notes[result[i].id] = result[i];
+        if (result[i].flags.indexOf("root") != -1)
+          rootNoteId = result[i].id;
+        rootNoteId
+      }
+      deferred.resolve()
+    });
+    return deferred.promise;
+  }
+
+  // Get the one to rule them all.
+  this.getRoot = function () {
+    return notes[rootNoteId];
+  }
+
+  // Get a note given its id.
+  this.getById = function (id) {
+    return notes[id];
+  }
+
+  // Get number of notes.
+  this.count = function () {
+    var size = 0, key;
+    for (key in notes) {
+        if (notes.hasOwnProperty(key)) size++;
+    }
+    return size;
+  }
 });
--- test/spec/services/notes.js
+++ test/spec/services/notes.js
@@ -6,16 +6,50 @@ describe('Service: notes', function () {
   beforeEach(module('dyanote'));
 
   // instantiate service
-  var notes, auth;
-  beforeEach(inject(function (_notes_, _auth_) {
+  var notes, auth, $httpBackend;
+  var note1, note4;
+
+  beforeEach(inject(function (_notes_, _auth_, _$httpBackend_) {
     notes = _notes_;
     auth = _auth_;
+
+    $httpBackend = _$httpBackend_;
+
+    // Mock data
     spyOn(auth, 'isAuthenticated').andReturn(true);
-    spyOn(auth, 'getEmail').andReturn('matteo@example.com');
+    spyOn(auth, 'getEmail').andReturn('user@example.com');
+
+    note1 = {
+      url: "<url>", id: 1, parent: "<parent-url>", created: "2013-12-24T17:41:10.871Z", flags: ["root"],
+      title: "<title>", body: "<note>note content</note>", author: "http://dyanote.herokuapp.com/api/users/user@example.com/"
+    };
+
+    note4 = {
+      url: "<url>", id: 4, parent: "<parent-url>", created: "2013-12-24T17:41:10.871Z", flags: [],
+      title: "<title2>", body: "<note>note content 2</note>", author: "http://dyanote.herokuapp.com/api/users/user@example.com/"
+    };
+
+    $httpBackend.expect('GET', 'https://dyanote.herokuapp.com/api/users/user@example.com/pages')
+      .respond(200, [note1, note4]);
   }));
 
-  it('should do something', function () {
-    expect(!!notes).toBe(true);
+  it('should load notes from server', function () {
+    notes.loadAll();
+    $httpBackend.flush();
+    expect(notes.count()).toBe(2);
   });
 
+  it('should allow to get notes by id', function () {
+    notes.loadAll();
+    $httpBackend.flush();
+    expect(JSON.stringify(notes.getById(1))).toEqual(JSON.stringify(note1));
+    expect(JSON.stringify(notes.getById(4))).toEqual(JSON.stringify(note4));
+    expect(notes.getById(42)).toBeUndefined();
+  });
+
+  it('should allow to get root note', function () {
+    notes.loadAll();
+    $httpBackend.flush();
+    expect(JSON.stringify(notes.getRoot())).toEqual(JSON.stringify(note1));
+  });
 });
