065dcb6698bd474df2ec0e01ca7a6da1773f9d6c
Matteo Nardi <commits@matteonardi.org>
2014-02-15 16:30:42 +0100

    Missing files in previous commit

---
+++ app/scripts/services/noteResource.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('dyanote')
+
+// noteResource handles comunication with the REST service.
+// It offers a CRUD interface and it doesn't keep track of notes.
+.service('noteResource', function ($http, $q, $log, auth, SERVER_CONFIG) {
+
+  // Load all notes from server with the currently logged in user.
+  // Returns a promise resolved with the note list.
+  this.getAll = function () {
+    if(!auth.isAuthenticated())
+      return $q.reject("User is not logged in");
+
+    var url = SERVER_CONFIG.apiUrl + 'users/' + auth.getEmail() + '/pages/';
+    return $http.get(url).then(function (result) {
+      return result.data;
+    });
+  }
+
+  // Create note.
+  // Returns a promise resolved with the saved note
+  // (which will contain a valid id)
+  this.post = function (note) {
+    var url = SERVER_CONFIG.apiUrl + 'users/' + auth.getEmail() + '/pages/';
+    return $http.post(url, note).then(function (result) {
+      return result.data;
+    });
+  }
+
+  // Update note.
+  this.put = function (note) {
+    $log.info("Updating note " + note.id);
+    return $http.put(note.url, note);
+  }
+});
---
+++ test/spec/services/noteResource.js
@@ -0,0 +1,120 @@
+'use strict';
+
+describe('Service: notes', function () {
+
+  // load the service's module
+  beforeEach(module('dyanote'));
+
+  // instantiate service
+  var noteResource,
+    auth,
+    SERVER_CONFIG,
+    $httpBackend,
+    $http,
+    $rootScope,
+    $log,
+    rootNote,
+    archiveNote,
+    note4,
+    authorUrl;
+
+  beforeEach(inject(function (_noteResource_, _auth_, _$httpBackend_, _$http_, _$rootScope_, _$log_, _SERVER_CONFIG_) {
+    noteResource = _noteResource_;
+    auth = _auth_;
+    SERVER_CONFIG = _SERVER_CONFIG_;
+
+    $log = _$log_;
+    $http = _$http_;
+    $httpBackend = _$httpBackend_;
+    $rootScope = _$rootScope_;
+
+    // Mock auth service
+    spyOn(auth, 'isAuthenticated').andReturn(true);
+    spyOn(auth, 'getEmail').andReturn('user@example.com');
+
+    // Create a set of notes.
+    authorUrl = SERVER_CONFIG.apiUrl + 'users/user@example.com/';
+    var date = '2013-12-24T17:41:10.871Z';
+    rootNote = {
+      url: authorUrl + 'pages/1/',
+      id: 1,
+      parent: '',
+      created: date,
+      flags: ['root'],
+      title: 'Root',
+      body: '<note>Root note</note>',
+      author: authorUrl
+    };
+    archiveNote = {
+      url: authorUrl + 'pages/2/',
+      id: 2,
+      parent: '',
+      created: date,
+      flags: ['archive'],
+      title: 'Archive',
+      body: '<note>Archive note</note>',
+      author: authorUrl
+    };
+    note4 = {
+      url: authorUrl + 'pages/4/',
+      id: 4,
+      parent: rootNote.url,
+      created: date,
+      flags: [],
+      title: 'Note 4',
+      body: '<note>Note 4</note>',
+      author: authorUrl
+    };
+  }));
+
+  it('should load notes from server', function () {
+    var count = 0;
+    $httpBackend.expect('GET', authorUrl + 'pages/')
+      .respond(200, [rootNote, archiveNote, note4]);
+
+    noteResource.getAll().then(function (notes) {
+      count = notes.length;
+    });
+    $rootScope.$apply();
+    $httpBackend.flush();
+    expect(count).toBe(3);
+  });
+
+  it('should fail if user is not logged in', function () {
+    auth.isAuthenticated.andReturn(false);
+    var promise = noteResource.getAll();
+    var msg = 'No error';
+    promise.then(null, function (reason) {
+      msg = reason;
+    });
+    $rootScope.$apply();
+    expect(msg).toEqual('User is not logged in');
+  });
+
+  it('should upload notes to server', function () {
+    $httpBackend.expect('PUT', rootNote.url).respond(200);
+    noteResource.put(rootNote);
+
+    $httpBackend.flush();
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest()
+  });
+
+  it('should create new notes', function () {
+    var uncompletedNote4 = JSON.parse(JSON.stringify(note4));
+    delete uncompletedNote4.id;
+    delete uncompletedNote4.url;
+    var completedNote4;
+    $httpBackend.expect('POST', authorUrl + 'pages/').respond(note4);
+    noteResource.post(uncompletedNote4).then(function (note) {
+      completedNote4 = note;
+    });
+    $httpBackend.flush();
+    $rootScope.$apply();
+    expect(completedNote4.id).toEqual(note4.id);
+    expect(completedNote4).toEqual(note4);
+
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest()
+  });
+});
