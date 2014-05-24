6366af8d58718f20c995f58a77b4d09b09476544
Matteo Nardi <commits@matteonardi.org>
2014-05-24 09:23:08 +0200

    Remove unused toolbar controller code

--- app/scripts/scribe/commands/link.js
+++ app/scripts/scribe/commands/link.js
@@ -54,65 +54,18 @@ dyanote.scribe.commands.link = function (scribe) {
   //   enabled: wheather the command can be executed
   //   error: error message
   //   data: {
+  //     title: title on the note which should be created
   //   }
   // }
   command.analyze = function () {
     var range = window.getSelection().getRangeAt(0);
-    var A = range.startContainer;
-    var B = range.endContainer;
-    var startOffset = range.startOffset;
-    var endOffset = range.endOffset;
-    var ancestor = range.commonAncestorContainer;
-    var affectedNodes = [];
 
-    // We want A and B to be ELEMENT nodes.
-    if (A.nodeType == utils.TEXT_NODE) {
-      startOffset = utils.getOffset(A);
-      A = A.parentNode;
-    }
-    if (B.nodeType == utils.TEXT_NODE) {
-      endOffset = utils.getOffset(B) + 1;
-      B = B.parentNode;
-    }
-    if (ancestor.nodeType == utils.TEXT_NODE) {
-      ancestor = ancestor.parentNode;
-    }
-
-
-    // Execution
-    try {
-      // Check range extremes are contained in the scribe editor.
-      if (!scribe.el.contains(A))
-        throw 'Extreme A is out of Scribe editor';
-      if (!scribe.el.contains(B))
-        throw 'Extreme B is out of Scribe editor';
-      
-      /*
-      checkAllDescendants(A.childNodes[startOffset]);
-      checkAllDescendants(B.childNodes[endOffset -1]);
-
-      var offsetA = checkUp(A, startOffset, +1, ancestor);
-      var offsetB = checkUp(B, endOffset,   -1, ancestor);
-
-      // Check all descendants of ancestor between A and B
-      for (var i = offsetA + 1; i < offsetB - 1; i++) {
-        checkAllDescendants(ancestor.childNodes[i]);
+    return {
+      enabled: true,
+      error: undefined,
+      data: {
+        title: range.toString()
       }
-      */
-
-      return {
-        enabled: true,
-        error: undefined,
-        data: {
-          title: range.toString()
-        }
-      };
-    } catch (error) {
-      return {
-        enabled: false,
-        error: error,
-        data: undefined
-      };
-    }
+    };
   };
 };
