42924c015a9ac88e476ac27e554e9ebb6fd5b4bc
Matteo Nardi <commits@matteonardi.org>
2015-01-07 16:20:54 +0100

    Add less compilation to Gulp

--- gulp/server.js
+++ gulp/server.js
@@ -4,39 +4,55 @@ var gulp = require('gulp'),
   del = require('del'),
   es = require('event-stream'),
   express = require('express'),
-  open = require('open');
+  open = require('open'),
+  less = require('gulp-less');;
 
 var sources = require('./config.json').sources;
 
 gulp.task('server', function () {
   var app = express();
   app.use(express.static('dist'));
-  // app.use(function (req, res) {
-  //   res.redirect('index.html');
-  // });
+  app.use(function (req, res) {
+    res.redirect('/');
+  });
   app.listen(9000);
   open('http://localhost:9000/');
 });
 
 gulp.task('build', [
-  'clean',
+  'index',
+  'style',
   'js:vendor',
   'js:dyanote',
-  'index'
+  'watch'
 ]);
 
 gulp.task('watch', function () {
   gulp.watch(sources.vendor, ['js:vendor']);
   gulp.watch(sources.dyanote.concat(sources.templates), ['js:dyanote']);
+  gulp.watch(sources.index, ['index']);
+  gulp.watch(sources.styles, ['style']);
 });
 
-gulp.task('js:vendor', ['clean'], function () {
+gulp.task('index', function () {
+  gulp.src(sources.index)
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('style', function () {
+  gulp.src(sources.styles)
+    .pipe(concat('style.less'))
+    .pipe(less())
+    .pipe(gulp.dest('dist/build'))
+})
+
+gulp.task('js:vendor', function () {
   gulp.src(sources.vendor)
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('dist/build'))
 });
 
-gulp.task('js:dyanote', ['clean'], function () {
+gulp.task('js:dyanote', function () {
   var js = gulp.src(sources.dyanote);
   var templates = gulp.src(sources.templates).pipe(templateCache({
     module: 'dyanote'
@@ -44,13 +60,4 @@ gulp.task('js:dyanote', ['clean'], function () {
   es.merge(js, templates)
     .pipe(concat('dyanote.js'))
     .pipe(gulp.dest('dist/build'));
-});
-
-gulp.task('index', ['clean'], function () {
-  gulp.src(sources.index)
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', function (cb) {
-  del(['dist'], cb);
 });
