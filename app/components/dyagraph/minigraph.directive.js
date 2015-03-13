8f2f7c202034b8ae5db3995ac0029493a05365a7
Matteo Nardi <commits@matteonardi.org>
2015-03-14 00:30:23 +0100

    Minigraph: add note focusing effect

--- app/components/dyagraph/minigraph.directive.js
+++ app/components/dyagraph/minigraph.directive.js
@@ -12,33 +12,86 @@ class Minigraph {
     this.scope = scope;
     this.element = element;
 
-    this.svg = d3.select(element[0]).append("svg").append('g');
+    this.svg = d3.select(element[0]).append('svg');
+    this.setupFilters();
+
+    // Render on openNotes changes
     scope.$watchCollection(() => this.openNotes.notes, () => this.render());
-    // scope.$watch(() => {return this.openNotes.notes.length}, () => this.render());
+
+    // Animate note focusing
+    var focusHandler = this.onNoteFocused.bind(this);
+    this.openNotes.addFocusHandler(focusHandler);
+    element.on('$destroy', function () {
+      this.openNotes.removeFocusHandler(focusHandler);
+    });
+  }
+
+  setupFilters () {
+    var defs = this.svg.append('defs');
+
+    var filter = defs.append('focusFilter')
+      .attr('id', 'drop-shadow')
+      .attr('height', '130%');
+
+    filter.append('feGaussianBlur')
+      .attr('in', 'SourceAlpha')
+      .attr('stdDeviation', 5)
+      .attr('result', 'blur');
+
+    // filter.append('feOffset')
+    //   .attr('in', 'blur')
+    //   .attr('dx', 5)
+    //   .attr('dy', 5)
+    //   .attr('result', 'offsetBlur');
+
+    var feMerge = filter.append('feMerge');
+    feMerge.append('feMergeNode')
+      .attr('in', 'blur')
+    feMerge.append('feMergeNode')
+      .attr('in', 'SourceGraphic');
   }
 
   render () {
     console.info('render');
-    var width = 300,
+    var me = this,
+      width = 300,
       height = 300,
       margin = 10;
 
-    var mainCircles = this.svg.selectAll('circle.open')
+    var mainCircles = this.svg.selectAll('.open')
       .data(this.openNotes.notes);
 
     mainCircles.enter().append('circle')
       .attr('class', 'open')
       .attr('r', '4')
-      .attr('cy', '30');
+      .attr('cy', '30')
+      .on('click', function (note) { me.onNoteClicked(this, note); })
+      .append('title');
 
     mainCircles
-      .attr('cx', (note, i) => margin + i * 30 );
+      .attr('cx', (note, i) => margin + i * 30 )
+      .attr('title', (note, i) => note.title )
+      .select('title')
+        .text((note, i) => note.title);
 
     mainCircles.exit().remove();
   }
 
-  onNodeClicked () {
-    // Todo
+  onNoteClicked (el, note) {
+    this.openNotes.focus(note);
+    this.scope.$digest();
+  }
+
+  onNoteFocused (note) {
+    console.log('onNoteFocused', note);
+    this.svg.selectAll('.open')
+      .filter(data => data === note)
+      .transition()
+        .duration(200)
+        .attr('r', '5')
+      .transition()
+        .duration(200)
+        .attr('r', '4')
   }
 }
 
