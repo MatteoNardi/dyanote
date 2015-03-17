bdd535a8f27eadd99535abfd4ebb808cc9797e3d
Matteo Nardi <commits@matteonardi.org>
2015-03-17 20:49:37 +0100

    Add child notes in minigraph

--- app/components/dyagraph/minigraph.directive.js
+++ app/components/dyagraph/minigraph.directive.js
@@ -27,52 +27,94 @@ class Minigraph {
   }
 
   render () {
-    this.renderOpenNotes();
+    this.renderNotes();
     this.renderPath();
   }
 
-  renderOpenNotes () {
-    var me = this;
+  renderNotes() {
+    var me = this, 
+      open = this.openNotes.notes,
+      children = open.map((n) => n.children),
+      all = children.reduce((output, current) => {
+        return (output || []).concat(...current);
+      }, [open[0]]);
 
-    var notes = this.svg.select('.notes').selectAll('.open')
-      .data(this.openNotes.notes);
+    // Join data
+    var openSet = new Set(open);
+    var selection = this.svg.select('.notes').selectAll('circle').data(all);
 
-    notes.enter().append('circle')
-      .attr('class', 'open')
+    // Enter
+    selection.enter().append('circle')
       .attr('r', '4')
-      .attr('cx', (note, i) => me.defaultPos(i).x)
-      .attr('cy', (note, i) => me.defaultPos(i).y)
-      .attr('fill',  this.defaultFill)
       .on('click', function (note) { me.onNoteClicked(this, note); })
       .append('title');
 
-    notes
-      .attr('title', (note, i) => note.title )
+    // Update
+    selection
+      .attr('title', (note, i) => note.title)
       .select('title')
         .text((note, i) => note.title);
+    this.updateOpenNotes(selection.filter(note => openSet.has(note)));
+    this.updateChildNotes(selection.filter(note => !openSet.has(note)));    
 
-    notes.exit().remove();
+    // Exit
+    selection.exit().remove();
+  }
+
+  // Update notes in selection to match "Open" style
+  updateOpenNotes (selection) {
+    var me = this;
+    selection
+      .attr('class', 'open')
+      .attr('fill',  this.defaultFill)
+      .transition()
+        .attr('cx', (note, i) => me.defaultPos(i).x)
+        .attr('cy', (note, i) => me.defaultPos(i).y);
+  }
+
+  // Update notes in selection to match "Child" style
+  updateChildNotes (selection) {
+    var me = this;
+    var getPos = (note) => {
+      var parent = this.openNotes.notes.indexOf(note.parent);
+      if (parent == -1) throw new Error (`Note ${note.title} has no parent open`);
+      var { x, y } = me.defaultPos(parent);
+      var radius = 25,
+        pseudoRandomNumer = (note.id % 10)/10,
+        baseAngle = note.id % 2 ? Math.PI/4 : Math.PI*5/4,
+        angle = baseAngle + (pseudoRandomNumer - .5) * Math.PI/3;
+      return {
+        x: x + radius * Math.cos(angle),
+        y: y - radius * Math.sin(angle)
+      }
+    };
+
+    selection
+      .attr('class', 'child')
+      .transition()
+        .attr('cx', note => getPos(note).x)
+        .attr('cy', note => getPos(note).y);
   }
 
   renderPath () {
     var me = this;
 
-    // var line = d3.svg.line()
-    //   .x(function(d) { return me.defaultPos(i).x; })
-    //   .y(function(d) { return me.defaultPos(i).y; })
-
-    var path = this.svg.select('.mainPath').selectAll('.path')
+    var paths = this.svg.select('.mainPath').selectAll('.path')
       .data(this.openNotes.notes);
 
-    path.enter().append('line')
+    paths.enter().append('path')
       .attr('class', 'path')
-      .attr('x1', (note, i) => me.defaultPos(i).x)
-      .attr('y1', (note, i) => me.defaultPos(i).y)
-      .attr('x2', (note, i) => me.defaultPos(i-1).x)
-      .attr('y2', (note, i) => me.defaultPos(i-1).y)
-      // .attr('d', line);
+      .attr('d', (note, i) => {
+        if (i == 0) return;
+        var source = me.defaultPos(i-1),
+          dest = me.defaultPos(i);
+        return `M${source.x},${source.y} ` +        // Move to source
+               `C${source.x+25},${source.y+25} ` +  // First control point 
+                 `${dest.x-25},${dest.y-25} ` +     // Second control point
+                 `${dest.x},${dest.y}`;             // Destination
+      });
     
-    path.exit().remove();
+    paths.exit().remove();
   }
 
   // Default note fill for open note circles
@@ -86,14 +128,17 @@ class Minigraph {
   // Default open note circles position
   defaultPos (i) {
     return {
-      x: i * 45 + (i%2 ? -5 : +5),
-      y: i * 45 + (i%2 ? +5 : -5)
+      x: i * 45 + (i%2 ? -12 : +12),
+      y: i * 45 + (i%2 ? +12 : -12)
     }
   }
 
   // Open note circles click handler
   onNoteClicked (el, note) {
-    this.openNotes.focus(note);
+    if (this.openNotes.notes.indexOf(note) == -1)
+      this.openNotes.open(note);
+    else
+      this.openNotes.focus(note);
     this.scope.$digest();
   }
 
@@ -103,16 +148,16 @@ class Minigraph {
     this.svg.selectAll('.open')
       .attr('fill', this.defaultFill)
       .filter(data => data === note)
-      .transition()
-        .duration(200)
-        .attr('r', '3')
-        .attr('fill', '#07a3d4') // Dyanote blue
-      .transition()
-        .duration(200)
-        .attr('r', '4');
+      // .transition()
+      //   .duration(200)
+      //   .attr('r', '3')
+      //   .attr('fill', '#07a3d4') // Dyanote blue
+      // .transition()
+      //   .duration(200)
+      //   .attr('r', '4');
 
     // Translate view
-    var i = this.openNotes.notes.indexOf(note) -2,
+    var i = this.openNotes.notes.indexOf(note) -1,
       pos = this.defaultPos(i > 0 ? i : 0);
     this.svg.select('.main')
       .transition()
