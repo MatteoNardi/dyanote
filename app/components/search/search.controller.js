8e7c1f6c0d4165b9f27af32e07883df01ab972d3
Matteo Nardi <commits@matteonardi.org>
2015-11-09 00:02:37 +0100

    More work on new search widget

--- app/components/search/search.controller.js
+++ app/components/search/search.controller.js
@@ -8,12 +8,13 @@ class SearchController {
     this.$log = $log;
     this.notesGraph = notesGraph;
     this.openNotes = openNotes;
+    this.activate();
   }
 
   activate () {
     this.searchTerms = '';
     this.isLoading = false;
-    this.results = []
+    this.results = [];
 
     this.$rootScope.$watch(() => this.searchTerms, text => {
       if (!text) {
@@ -22,22 +23,44 @@ class SearchController {
         return;
       }
       this.$log.info('Searching for "' + text + '"');
-      var response = this.notesGraph.search(text);
       this.isLoading = true;
-      console.log(response.results);
-      this.results = response.results;
-      response.promise.then(() => this.isLoading = false);
+      // Todo: case insensitive
+      var contains = x => x && x.indexOf(text) != -1;
+      var response = this._search(note =>
+        contains(this.notesGraph.body(note)) ||
+        contains(this.notesGraph.title(note))
+      );
     });
   }
 
-  open (note) {
-    console.log(note);
-    this.openNotes.open(note);
-    this.$location.path('/notes/view');
+  _search (predicate) {
+    if (this.runningSearch) {
+      clearTimeout(this.runningSearch);
+      this.results.length = 0;
+      console.info('clear', this.results);
+    }
+    var graph = this.notesGraph;
+    var queue = graph.roots();
+    console.warn('serach', queue);
+    var digest = _ => this.$rootScope.$$phase || this.$rootScope.$apply();
+    var process = _ => {
+      var item = queue.shift();
+      if (item === undefined) return;
+      queue = queue.concat(graph.children(item));
+      if (predicate(item) && this.results.indexOf(item) == -1) {
+        this.results.push(item);
+        digest();
+        console.info('change', this.results);
+      }
+      if (queue.length) this.runningSearch = setTimeout(process, 0);
+    };
+    process();
+    // on end: this.isLoading = false;
   }
 
-  cancel () {
-    this.$location.path('/notes/view');
+  open (note) {
+    // console.log(note);
+    this.openNotes.open(note);
   }
 }
 
